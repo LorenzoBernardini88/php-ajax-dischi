79b1892254aced314ec419b72024213a757dada9
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-02-17 15:32:52 +0100

    milestone 1

---
+++ milestone1/index.php
@@ -0,0 +1,82 @@
+<?php
+$database = [
+    [
+        'title' => 'New Jersey',
+        'author' => 'Bon Jovi',
+        'year' => 1988,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
+        'genre' => 'Rock'
+    ],
+    [
+        'title' => 'Live at Wembley 86',
+        'author' => 'Queen',
+        'year' => 1992,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
+        'genre' => 'Pop'
+    ],
+    [
+        'title' => 'Ten\'s Summoner\'s Tales',
+        'author' => 'Sting',
+        'year' => 1993,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
+        'genre' => 'Pop'
+    ],
+    [
+        'title' => 'Steve Gadd band',
+        'author' => 'Steve Gadd Band',
+        'year' => 2018,
+        'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
+        'genre' => 'Jazz'
+    ],
+    [
+        'title' => 'Brave new World',
+        'author' => 'Iron Maiden',
+        'year' => 2000,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
+        'genre' => 'Metal'
+    ],
+    [
+        'title' => 'One more car, one more rider',
+        'author' => 'Eric Clapton',
+        'year' => 2002,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
+        'genre' => 'Rock'
+    ]
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Milestone1</title>
+</head>
+<body>
+    <header class="head_cont">
+        <div class="logo_cont">
+            <img src="spotify_logo.png" alt="">
+        </div>
+    </header>
+
+    <div class="general" >
+        <div  class="container">
+                <?php
+                foreach($database as $key => $valore){
+                    echo '<div class="card">';
+                        echo '<img src="'.$valore["poster"].'"/>';
+                        echo '<span class="album_title" >'.$valore["title"].'</span>';
+                        echo '<span class="album_author" >'.$valore["author"].'</span>';
+                        echo '<span class="album_year" >'.$valore["year"].'</span>';
+                    echo '</div>';
+                }
+                ?>
+        </div>
+    </div>
+</body>
+</html>
+        
+        
+        
+    
